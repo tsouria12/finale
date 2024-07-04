329b3f227a8f372a3b05a3ada0bbab3c771173d7
tsouria12 <tsouristanos@outlook.com>
2024-07-04 20:43:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -127,7 +127,24 @@ bot.on('callback_query', (callbackQuery) => {
     userData[chatId] = { state: STATES.SELECTING_CHAIN };
   }
 
-  if (userData[chatId].state === STATES.SELECTING_CHAIN) {
+  if (data === 'confirm_delete') {
+    const deleteMessageId = userData[chatId].deleteMessageId || messageId;
+    delete userData[chatId]; // Clear user data
+    const opts = {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: 'ETH', callback_data: 'ETH' }],
+          [{ text: 'BNB', callback_data: 'BNB' }],
+          [{ text: 'SOL', callback_data: 'SOL' }]
+        ]
+      }
+    };
+    editMessage(chatId, deleteMessageId, 'Select chain:', opts);
+    logger.info('All configuration data has been deleted.');
+  } else if (data === 'cancel_delete') {
+    bot.sendMessage(chatId, 'Deletion cancelled.');
+    logger.info('Deletion cancelled.');
+  } else if (userData[chatId].state === STATES.SELECTING_CHAIN) {
     userData[chatId].chain = data;
     userData[chatId].state = STATES.TYPING_TOKEN;
     editMessage(chatId, messageId, 'Send me token address.');
@@ -231,25 +248,73 @@ Be sure to read full message before you continue, by clicking "✅ Confirm" butt
         ]
       }
     };
-    editMessage(chatId, messageId, 'Select Chain:', opts);
+    editMessage(chatId, messageId, 'Order cancelled. Starting over.', opts);
     logger.info('Order cancelled and starting over.');
-  } else if (data === 'confirm_delete') {
-    const deleteMessageId = userData[chatId].deleteMessageId || messageId;
-    delete userData[chatId]; // Clear user data
+  } else if (userData[chatId].state === STATES.SELECTING_CHAIN) {
+    userData[chatId].chain = data;
+    userData[chatId].state = STATES.TYPING_TOKEN;
+    editMessage(chatId, messageId, 'Send me token address.');
+    logger.info(`Chain selected: ${data}`);
+  } else if (userData[chatId].state === STATES.SELECTING_SLOT) {
+    if (data === 'Fast-Track') {
+      userData[chatId].order = data;
+      userData[chatId].state = STATES.TYPING_PORTAL;
+      editMessage(chatId, messageId, '❔ Send me portal/group link.');
+      logger.info(`Order selected: ${data}`);
+    } else {
+      userData[chatId].slot = data;
+      userData[chatId].state = STATES.SELECTING_PERIOD;
+      const opts = {
+        reply_markup: {
+          inline_keyboard: [
+            [{ text: '3 hours', callback_data: '3 hours' },
+             { text: '6 hours | -10%', callback_data: '6 hours' }],
+            [{ text: '12 hours | -20%', callback_data: '12 hours' },
+             { text: '24 hours | -30%', callback_data: '24 hours' }]
+          ]
+        }
+      };
+      editMessage(chatId, messageId, '❔ Select period:', opts);
+      logger.info(`Slot selected: ${data}`);
+    }
+  } else if (userData[chatId].state === STATES.SELECTING_PERIOD) {
+    userData[chatId].period = data;
+    userData[chatId].state = STATES.CONFIRMING_ORDER;
+    const { chain, token_address, portal_link, slot, period } = userData[chatId];
+    const price = PRICES[slot][period];
+
+    const confirmationMessage = `
+<b>Confirm your order:</b>
+
+<b>Token Address:</b> <b>${token_address}</b>
+<b>Chain:</b> <b>${chain}</b>
+<b>Portal:</b> <b>${portal_link}</b>
+<b>Time:</b> <b>${period}</b>
+<b>Top:</b> <b>${slot}</b>
+<b>Price:</b> <b>${price} SOL</b>
+
+Be sure to read full message before you continue, by clicking "✅ Confirm" button below you also confirm that you understand and accept rules:
+1. Deluge.Cash team can remove your token from the trending list with no chance of a refund if they suspect a scam in your token (ex.: sell tax 99%, developer mints a lot of tokens, liquidity removal and etc.) or abandoned project or lack of telegram group moderation or false information or deception or NSFW content in group or any place where links in channel/group leads to including "portals" to group.
+2. You must ensure that your advertisement, links to channels or groups you provide and any related materials posted, distributed or linked to in a group or channel you provide do not provide false information, deception, sexual or any NSFW (Not Safe For Work) content. This includes, but is not limited to, any material that is pornographic, sexually explicit, or otherwise inappropriate for a general audience.
+3. You are forbidden from including or linking to pornography, sexually explicit images, videos, or other materials, whether real or simulated, in your advertisement.
+4. You must avoid including sexually suggestive content in your advertisement, including images, videos, text, and any other forms of media intended to arouse.
+5. You must ensure that your advertisement do not involve scams or fraudulent schemes intended to deceive others for financial gain or other benefits.
+6. If suspicious activity in the form of "farming" (developers keeping more than 14%, splitting wallets) is noticed and according to the Deluge.Cash team it may be a threat, your token will be removed from trending list, refund is not available.
+7. You should also realize that the position in the trending list has NO IMPACT on the chances of sending a buy in the trending channel, chances of sending buy to channel: ~25% for buys >10$ if @buybot setted up in group.
+8. For violation of any of the above rules your token will be removed from trending list, refund is not available.
+9. Refund can be made only in case of full service disruption (stop updating trending list and your token not in the list and full stop displaying buys in the channel) more than 20 minutes straight and to the address of the wallet from which the payment was made to the address for payment, do NOT send payment from exchanges or wallets to which you do not have access because you will not be refunded, use only your personal wallet to which you will always have access.
+`;
     const opts = {
       reply_markup: {
         inline_keyboard: [
-          [{ text: 'ETH', callback_data: 'ETH' }],
-          [{ text: 'BNB', callback_data: 'BNB' }],
-          [{ text: 'SOL', callback_data: 'SOL' }]
+          [{ text: '✅ Confirm', callback_data: 'confirm_order' }],
+          [{ text: '🔄 Cancel and start over', callback_data: 'cancel_and_start_over' }]
         ]
-      }
+      },
+      parse_mode: 'HTML'
     };
-    editMessage(chatId, deleteMessageId, 'Select chain:', opts);
-    logger.info('All configuration data has been deleted.');
-  } else if (data === 'cancel_delete') {
-    bot.sendMessage(chatId, 'Deletion cancelled.');
-    logger.info('Deletion cancelled.');
+    editMessage(chatId, messageId, confirmationMessage, opts);
+    logger.info(`Period selected: ${data}`);
   }
 });
 
